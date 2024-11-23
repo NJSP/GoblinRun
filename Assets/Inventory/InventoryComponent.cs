ae7085b35aa3a2df20bf59ff3ba0501a6bf3ece0
NJSP <Njpetrone@outlook.com>
2024-11-23 11:24:42 -0800

    Fixed pick-up and drop mechanics

--- Assets/Inventory/InventoryComponent.cs
+++ Assets/Inventory/InventoryComponent.cs
@@ -1,130 +1,138 @@
-﻿using System;
+﻿using Assets.Inventory;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using UnityEngine;
 
-namespace Assets.Inventory
+internal class InventoryComponent : MonoBehaviour
 {
-    internal class InventoryComponent : MonoBehaviour
+    private List<ItemSO> inventory;
+    private List<ItemPickup> overlappingItems = new List<ItemPickup>();
+    public ObjectPool itemPool; // Reference to the ObjectPool
+
+    void Start()
     {
-        private SortedSet<ItemSO> inventory;
-        private List<ItemPickup> overlappingItems = new List<ItemPickup>();
+        inventory = new List<ItemSO>();
+    }
 
-        void Start()
+    public void AddItem(string name, int value)
+    {
+        ItemSO newItem = ScriptableObject.CreateInstance<ItemSO>();
+        newItem.itemName = name;
+        newItem.value = value;
+        inventory.Add(newItem);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        ItemPickup item = other.GetComponent<ItemPickup>();
+        if (item != null && !overlappingItems.Contains(item))
         {
-            inventory = new SortedSet<ItemSO>(new ItemComparer());
-        }
-
-        public void AddItem(string name, int value)
-        {
-            ItemSO newItem = new ItemSO { itemName = name, value = value };
-            inventory.Add(newItem);
-        }
-
-        void OnTriggerEnter(Collider other)
-        {
-            // Check if the other object has an ItemPickup component
-            ItemPickup item = other.GetComponent<ItemPickup>();
-            if (item != null)
-            {
-                overlappingItems.Add(item); // Add the item to the overlapping list
-            }
-        }
-
-        void OnTriggerExit(Collider other)
-        {
-            // Remove the item from the overlapping list when it leaves the collider
-            ItemPickup item = other.GetComponent<ItemPickup>();
-            if (item != null)
-            {
-                overlappingItems.Remove(item);
-            }
-        }
-
-        void Update()
-        {
-            // Check for the 'E' key press
-            if (Input.GetKeyDown(KeyCode.E))
-            {
-                PickupItem();
-            }
-
-            // Check for the 'Q' key press
-            if (Input.GetKeyDown(KeyCode.Q))
-            {
-                DropLowestValueItem();
-            }
-
-            if (inventory.Min == null)
-            {
-                inventory.Remove(inventory.Min);
-
-            }
-
-            if (Input.GetKeyDown(KeyCode.I))
-            {
-                foreach (ItemSO item in inventory)
-                {
-                    Debug.Log($"Item: {item.itemName}, Value: {item.value}");
-                }
-            }
-        }
-        public void DropLowestValueItem()
-        {
-            if (inventory.Count > 0)
-            {
-                ItemSO lowestValueItem = inventory.Min;
-
-                // Assuming the prefab is stored in the ItemSO
-                GameObject itemPrefab = lowestValueItem.prefab;
-                if (itemPrefab != null)
-                {
-                    // Spawn the item on top of the player
-                    Instantiate(itemPrefab, transform.position + Vector3.up, Quaternion.identity);
-                }
-
-                inventory.Remove(lowestValueItem);
-                Debug.Log($"Dropped item: {lowestValueItem.itemName} with value {lowestValueItem.value}");
-            }
-            else
-            {
-                Debug.Log("No items to drop.");
-            }
-
-            RemoveNullItems();
-        }
-
-        private void PickupItem()
-        {
-            if (overlappingItems.Count > 0 && overlappingItems[0] != null)
-            {
-                // Pick the first item in the overlapping list
-                ItemPickup itemToPick = overlappingItems[0];
-                inventory.Add(itemToPick.itemData); // Add item to inventory
-                Debug.Log($"Picked up: {itemToPick.itemData.itemName}, Value: {itemToPick.itemData.value}");
-
-                // Destroy or deactivate the item GameObject
-                Destroy(itemToPick.gameObject);
-
-                // Remove it from the overlapping list
-                overlappingItems.Remove(itemToPick);
-            }
-            else
-            {
-                Debug.Log("No items to pick up.");
-            }
-            RemoveNullItems();
-
-        }
-
-        public void RemoveNullItems()
-        {
-            // Remove all null items
-            inventory.RemoveWhere(item => item == null);
-            Debug.Log("Removed null placeholders from the list.");
+            overlappingItems.Add(item);
+            Debug.Log($"Added to OverlappingItemsList: {item.itemData.itemName}");
         }
     }
-    
+
+    void OnTriggerExit(Collider other)
+    {
+        ItemPickup item = other.GetComponent<ItemPickup>();
+        if (item != null && overlappingItems.Contains(item))
+        {
+            overlappingItems.Remove(item);
+            Debug.Log($"Removed from OverlappingItemsList: {item.itemData.itemName}");
+        }
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            PickupItem();
+        }
+
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            DropLowestValueItem();
+        }
+
+        if (Input.GetKeyDown(KeyCode.I))
+        {
+            foreach (ItemSO item in inventory)
+            {
+                Debug.Log($"Item: {item.itemName}, Value: {item.value}");
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.Z))
+        {
+            foreach (ItemPickup item in overlappingItems)
+            {
+                Debug.Log($"Overlapping Item: {item.itemData.itemName}");
+            }
+        }
+    }
+
+    public void DropLowestValueItem()
+    {
+        if (inventory.Count > 0)
+        {
+            ItemSO lowestValueItem = GetLowestValueItem();
+            GameObject itemPrefab = lowestValueItem.prefab;
+            if (itemPrefab != null)
+            {
+                GameObject item = itemPool.GetObject();
+                item.transform.position = transform.position + Vector3.up;
+                item.transform.rotation = Quaternion.identity;
+                Rigidbody rb = item.GetComponent<Rigidbody>();
+                if (rb != null)
+                {
+                    rb.AddForce(Vector3.up * 5.0f, ForceMode.Impulse); // Add slight upward force
+                    rb.AddTorque(Random.insideUnitSphere * 5.0f, ForceMode.Impulse); // Add random torque for spinning
+                }
+
+                // Set the item data on the dropped GameObject
+                ItemPickup itemPickup = item.GetComponent<ItemPickup>();
+                if (itemPickup != null)
+                {
+                    itemPickup.itemData = lowestValueItem;
+                }
+            }
+
+            inventory.Remove(lowestValueItem);
+            Debug.Log($"Dropped item: {lowestValueItem.itemName} with value {lowestValueItem.value}");
+        }
+        else
+        {
+            Debug.Log("No items to drop.");
+        }
+    }
+
+    private ItemSO GetLowestValueItem()
+    {
+        ItemSO lowestValueItem = null;
+        foreach (var item in inventory)
+        {
+            if (lowestValueItem == null || item.value < lowestValueItem.value)
+            {
+                lowestValueItem = item;
+            }
+        }
+        return lowestValueItem;
+    }
+
+    private void PickupItem()
+    {
+        if (overlappingItems.Count > 0)
+        {
+            ItemPickup itemToPick = overlappingItems[0];
+            inventory.Add(itemToPick.itemData);
+            Debug.Log($"Picked up: {itemToPick.itemData.itemName}, Value: {itemToPick.itemData.value}");
+
+            itemPool.ReturnObject(itemToPick.gameObject);
+            overlappingItems.Remove(itemToPick);
+            Debug.Log($"Removed from OverlappingItemsList: {itemToPick.itemData.itemName}");
+        }
+        else
+        {
+            Debug.Log("No items to pick up.");
+        }
+    }
 }
